e3bbb1ac9c1f7cb75ef3e44ddb89432f9994ce17
david3080 <david3080@gmail.com>
2020-08-21 19:15:37 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,27 @@
 import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'home.dart';
 import 'login.dart';
 import 'resto.dart';
+
+  final FirebaseOptions firebaseOptions = const FirebaseOptions(
+    apiKey: "AIzaSyC690bkpcy8xkMrR0MIa37tOpJUqPFuA0k",
+    authDomain: "firestore-5643d.firebaseapp.com",
+    databaseURL: "https://firestore-5643d.firebaseio.com",
+    projectId: "firestore-5643d",
+    storageBucket: "firestore-5643d.appspot.com",
+    messagingSenderId: "241654013175",
+    appId: "1:241654013175:web:a8331ea10e1f96ae3e07f2",
+    measurementId: "G-WHYCJHVGCV"
+  );
+
+  Future<void> initializeDefault() async {
+    FirebaseApp app = await Firebase.initializeApp();
+    assert(app != null);
+  }
 
 Future<void> main() async {
   runApp(
@@ -29,6 +46,7 @@ class Landing extends StatefulWidget {
 class _LandingState extends State<Landing> {
   @override
   void initState() {
+    Firebase.initializeApp();
     Resto.initRestos();
     super.initState();
   }
